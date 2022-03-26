c23cd2aceb6319159806b35783a587521290ca91
Hassan <hassansmallah@gmail.com>
2022-03-26 15:04:26 +0300

    async

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World", "status": "venv installed"}
 
 
+# e.g. http://127.0.0.1:8000/items/5?q=This%20is%205
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
