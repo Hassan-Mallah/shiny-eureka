82fd1e512e78378e7621a9521b178fe250823184
Hassan <hassansmallah@gmail.com>
2022-03-27 09:58:52 +0300

    Token - api key

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ to run the app:
     
     $ pip install -r requirements.txt     
     $ uvicorn main:app --reload
+
+To-do:
+- Api key: https://nilsdebruin.medium.com/fastapi-authentication-revisited-enabling-api-key-authentication-122dc5975680
--- main.py
+++ main.py
@@ -1,8 +1,31 @@
+from fastapi import Security, Depends, FastAPI, HTTPException
+from fastapi.security.api_key import APIKeyHeader, APIKey
 from typing import Optional
-
-from fastapi import FastAPI
+from starlette.status import HTTP_403_FORBIDDEN
+from starlette.responses import RedirectResponse, JSONResponse
 
 app = FastAPI()
+
+API_KEY = "1234567asdfgh"
+API_KEY_NAME = "access_token"
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME)
+
+
+async def get_api_key(
+        api_key_header: str = Security(api_key_header),
+):
+    if api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
+
+
+@app.post("/auth/")
+async def read_items(api_key: APIKey = Depends(get_api_key)):
+    return {"token": 'token'}
 
 
 @app.get("/")
